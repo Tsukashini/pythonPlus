2cb2a38fab77c6b82a27d777f2b72de1f8659103
Tsukashini <veretig@yandex.by>
2023-01-07 01:36:52 +0300

    Homework 4 complete

---
+++ Homework3/methods.py
@@ -0,0 +1,29 @@
+"""
+# list methods
+list.append(elem)
+list.count(elem)
+list.sort(*, key=None, reverse=False)
+list.reverse()
+list.clear()
+
+# dict methods
+dict.items()
+dict.values()
+dict.keys()
+dict.get(key[, default])
+dict.pop(key[, default])
+
+# set methods
+set.add(elem)
+set.remove(elem)
+set.union(other, ...)
+set.difference(other, ...)
+set.intersection(other, ...)
+
+# str methods
+str.format(*args, **kwargs)
+str.split(elem)
+str.replace(__old='', __new='', __count=)
+str.find(str, [start],[end])
+str[start: end: step]
+"""
